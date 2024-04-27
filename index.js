c81d079fe57274501cc7c1d0bff85eaa9ad59af8
unknown <fabioflauzino@hotmail.com>
2024-04-27 15:30:52 -0300

    subindo codigo finalizado

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import entradaDados from 'readline-sync';
+
+/*** Coleção de dados ***/
+
 const questoes = [
   { id: 1, pergunta: 'Quando aconteceu o atentado as Torres Gêmeas?', resposta: '2001' },
   { id: 2, pergunta: 'Em que ano foi detectado o primeiro paciente com coronavírus?', resposta: '2019' },
@@ -31,23 +35,69 @@ const questoes = [
   { id: 30, pergunta: 'Em que ano o homem pisou na Lua pela primeira vez?', resposta: '1969' }
 ];
 
-function embaralhaArray(colecaoPerguntas){
-  return colecaoPerguntas.sort(() => Math.random() - 0.5);
+
+/*** Funções lógicas da aplicação ***/
+
+function selecionarQuestoesAleatorias(questoes, quantidadeQuestoes) { // Função que retorna 10 perguntas aleatorias
+  const questoesAleatorias = questoes.sort(() => Math.random() - 0.5); // Embaralha as perguntas do array questoes
+  return questoesAleatorias.slice(0, quantidadeQuestoes); // retorna um novo array com 10 perguntas aleatórias selecionadas
 }
 
-function selecionarPerguntasAleatorias(colecaoPerguntas, quantidade){
-  const perguntasEmbaralhadas = embaralhaArray(colecaoPerguntas)
-  return perguntasEmbaralhadas.slice(0, quantidade)
+function exibirPergunta(pergunta, index) { // Função que exibe a pergunta e retornar a resposta do usuário
+  console.log(`\n${index + 1}: ${pergunta.pergunta}`);
+  const respostaUsuario = entradaDados.questionInt('Digite a resposta (ano): ', {limit: '<0-9>', limitMessage: 'Digite apenas numeros'});
+  return respostaUsuario;
 }
 
-function exibriPerguntas(colecaoPerguntas){
-  colecaoPerguntas.forEach((pergunta, index) => {
-    console.log(`Pergunta ${index + 1}: ${pergunta.pergunta}`);
-  })
+function validarRespostaDoUsuario(respostaDoUsuario, pergunta) { // Função que valida a resposta do usuário
+  return respostaDoUsuario === pergunta.resposta;
 }
 
-const quantidadePerguntas = 10;
+function exibirResultado(pontuacaoFinal, nomeJogador) { // Função que retornar uma mensagem com base na pontuação do jogador
+  let mensagem = '';
 
-const perguntasSelecionadas = selecionarPerguntasAleatorias(questoes, quantidadePerguntas)
+  if (pontuacaoFinal <= 3) {
+    mensagem = 'OH NÃO! Tente mais uma vez.';
+  } else if (pontuacaoFinal <= 6) {
+    mensagem = 'BOM TRABALHO! Pratique um pouco mais.';
+  } else if (pontuacaoFinal <= 9) {
+    mensagem = 'MUITO BOM! Você acertou a maioria.';
+  } else {
+    mensagem = 'EXCELENTE! Você é um verdadeiro expert.';
+  }
 
-console.log(perguntasSelecionadas)
+  console.log(`\nJogador(a): ${nomeJogador}`);
+  console.log(`Pontuação final: ${pontuacaoFinal} ${pontuacaoFinal > 1 ? 'acertos' : 'acerto'}.`);
+  console.log(mensagem);
+}
+
+
+/*** Interface da aplicação ***/
+
+function iniciarQuiz() {
+  console.log('--------QUIZ DE FATOS HISTÓRICOS--------\n');
+  console.log('Seja Bem-vindo jogador(a)!');
+
+  const nomeJogador = entradaDados.question('Digite o seu nome: ');
+  const quantidadeQuestoes = 10;
+
+  const questoesSelecionadas = selecionarQuestoesAleatorias(questoes, quantidadeQuestoes);   // Armazena as 10 perguntas aleatórias
+
+  let pontuacaoFinal = 0;
+
+  questoesSelecionadas.forEach((pergunta, index) => {
+    const respostaUsuario = exibirPergunta(pergunta, index);
+    const resultadoValidacao = validarRespostaDoUsuario(respostaUsuario, pergunta);
+
+    if (resultadoValidacao) {
+      console.log('Resposta correta!');
+      pontuacaoFinal++;
+    } else {
+      console.log('Resposta errada!');
+    }
+  });
+
+  exibirResultado(pontuacaoFinal, nomeJogador);
+}
+
+iniciarQuiz(); // Inicia o Quiz
